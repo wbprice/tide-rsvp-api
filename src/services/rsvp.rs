fe62ef44352d43a0a5cd802153a4522530678061
Blaine Price <blaine@ario.com>
2020-09-07 22:28:32 -0400

    Wow, dynomite is great

--- src/services/rsvp.rs
+++ src/services/rsvp.rs
@@ -1,10 +1,9 @@
-use std::env;
-use std::collections::HashMap;
-use uuid::Uuid;
-use rusoto_dynamodb::{DynamoDbClient, DynamoDb, QueryInput, AttributeValue};
+use dynomite::{attr_map, FromAttributes, Item};
 use rusoto_core::Region;
-use dynomite::FromAttributes;
-use tide::{Result, Error, StatusCode};
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient, PutItemInput, QueryInput, GetItemInput};
+use std::env;
+use tide::{Error, Result, StatusCode};
+use uuid::Uuid;
 
 use crate::models::RSVP;
 
@@ -13,50 +12,45 @@ pub struct RSVPService;
 impl RSVPService {
     pub async fn get_by_household(&self, household_id: Uuid) -> Result<Vec<RSVP>> {
         let client = DynamoDbClient::new(Region::UsEast1);
+        let table_name = env::var("RSVP_TABLE_NAME")?;
 
-        // Query RSVP records to get records with a common household ID
-        let mut query = HashMap::new(); 
-        query.insert(
-            ":household_id".to_string(),
-            AttributeValue {
-                s: Some(household_id.to_string()),
-                ..AttributeValue::default()
-            }
-        );
-        
         // Make the request
-        let request = client.query(QueryInput {
-            table_name: env::var("RSVP_TABLE_NAME").unwrap(),
-            key_condition_expression: Some("household_id = :household_id".to_string()),
-            expression_attribute_values: Some(query),
-            ..QueryInput::default()
-        }).await;
+        let response = client
+            .query(QueryInput {
+                table_name,
+                key_condition_expression: Some("household_id = :household_id".to_string()),
+                expression_attribute_values: Some(attr_map! {
+                    ":household_id" => household_id.to_string()
+                }),
+                ..QueryInput::default()
+            })
+            .await?;
 
-        // Check the request
-        if let Ok(response) = request {
-            // If the request was successful, convert the response into an array of RSVP items
-            match response.items {
-                Some(items) => {
-                    let rsvps: Vec<RSVP> = items
-                        .into_iter()
-                        .map(|item| RSVP::from_attrs(item).unwrap())
-                        .collect();
-                    Ok(rsvps)
-                },  
-                None => {
-                    Ok(vec![])
-                }
+        // Map each item into an RSVP and return the list
+        match response.items {
+            Some(items) => {
+                let rsvps: Vec<RSVP> = items
+                    .into_iter()
+                    .map(|item| RSVP::from_attrs(item).unwrap())
+                    .collect();
+                Ok(rsvps)
             }
-        } else {
-            Err(Error::from_str(
-                StatusCode::InternalServerError, 
-                "Something bad happened".to_string())
-            )
+            None => Ok(vec![]),
         }
     }
 
-    pub async fn put(&self, id: Uuid, rsvp: RSVP) -> Result<RSVP> {
+    pub async fn put(&self, id: Uuid, rsvp: RSVP) -> Result<()> {
         let client = DynamoDbClient::new(Region::UsEast1);
+        let table_name = env::var("RSVP_TABLE_NAME")?;
 
+        client
+            .put_item(PutItemInput {
+                table_name: table_name.clone(),
+                item: rsvp.into(),
+                ..PutItemInput::default()
+            })
+            .await?;
+
+        Ok(())
     }
 }
